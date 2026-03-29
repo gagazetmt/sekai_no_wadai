8e9e184c574701ae459d964d1137f6b68cf3dbcf
gagazetmt <gagazetmt@github.com>
2026-03-29 23:56:10 +0900

    fix: tailscale upをバックグラウンド実行に変更（bat即時終了の修正）

--- 02_reddit_global/start_home_launcher.bat
+++ 02_reddit_global/start_home_launcher.bat
@@ -1,9 +1,8 @@
 @echo off
 cd /d "%~dp0"
 echo ⚽ ホームランチャー起動中...
-echo Tailscale接続中...
-tailscale up 2>nul
 echo スマホからのアクセス: http://100.115.192.114:3005
+start /b tailscale up >nul 2>&1
 start /b cmd /c "timeout /t 2 /nobreak >nul && start http://100.115.192.114:3005"
 node home_launcher.js
 pause
